2ff8024994750520d3341bdbae9bc53bd29d5634
DevmanYur <142353662+DevmanYur@users.noreply.github.com>
2023-09-06 09:03:51 +0000

    Добавлен шаблон и ф-я get_delta_year()

--- main.py
+++ main.py
@@ -1,5 +1,32 @@
 from http.server import HTTPServer, SimpleHTTPRequestHandler
 
+from jinja2 import Environment, FileSystemLoader, select_autoescape
+
+import datetime
+
+env = Environment(
+    loader=FileSystemLoader('.'),
+    autoescape=select_autoescape(['html', 'xml'])
+)
+
+template = env.get_template('template.html')
+
+
+def get_delta_year():
+    date_foundation = datetime.datetime(year=1920,month=1, day=1)
+    date_now = datetime.datetime.now()
+    delta = date_now - date_foundation
+    seconds = delta.total_seconds()
+    years = seconds / 60 / 60 / 24 / 365
+    return int(years)
+
+
+rendered_page = template.render(
+    delta_year = get_delta_year()
+)
+
+with open('index.html', 'w', encoding="utf8") as file:
+    file.write(rendered_page)
 
 server = HTTPServer(('0.0.0.0', 8000), SimpleHTTPRequestHandler)
 server.serve_forever()
